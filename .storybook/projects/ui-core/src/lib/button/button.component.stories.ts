8ca23984758a22bfc727cdbd183ee032326146a3
minervaux <82421444+minervaux@users.noreply.github.com>
2025-09-19 15:15:55 -0700

    Create button.component.stories.ts

---
+++ .storybook/projects/ui-core/src/lib/button/button.component.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { ButtonComponent } from './button.component';
+
+const meta: Meta<ButtonComponent> = {
+  title: 'Components/Button',
+  component: ButtonComponent,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<ButtonComponent>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    label: 'Primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    label: 'Secondary',
+  },
+};
